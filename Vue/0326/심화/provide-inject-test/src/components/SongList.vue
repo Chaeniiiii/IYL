3236fed89ae6c880283bfd1c903f4725a323f43b
chaeniiiii <kce2360@gmail.com>
2025-03-26 16:30:24 +0900

    [Vue] 0326 : 심화 문제

---
+++ Vue/0326/심화/provide-inject-test/src/components/SongList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <SongListItem v-for="s in songs" :key="s.id" :song="s" />
+
+    <div>체크된 곡 수 : {{ doneCount }}</div>
+  </ul>
+</template>
+
+<script>
+import SongListItem from './SongListItem.vue';
+export default {
+  name: 'SongList',
+  components: {
+    SongListItem,
+  },
+  props: ['songs'],
+  inject: ['doneCount'],
+};
+</script>
